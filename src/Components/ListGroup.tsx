c87be600bd4159d15d9ddff2f25947715ed34f0c
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-05-18 06:58:33 +0530

    updated list group component

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -5,12 +5,10 @@ const ListGroup = () => {
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+      {items.length === 0 && <p>No items allowed</p>}
+      {items.map((item) => {
+        return <li key={item}>{item}</li>;
+      })}
     </>
   );
 };
