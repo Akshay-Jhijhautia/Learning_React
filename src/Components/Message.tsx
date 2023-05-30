ae5ad0212226e331fb76ec96e13f2dc8f9cf1232
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-05-30 14:37:13 +0530

    added the message component

--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -1,9 +1,16 @@
-function Message() {
-  const name = "a";
-  if (name) {
-    return <span>Hello {name}</span>;
-  }
-  return <span>Hello World</span>;
-}
+import React from "react";
+
+const Message = () => {
+  const name = "";
+  if (name)
+    return (
+      <>
+        <h1>Hello {name}</h1>
+        <div>Welcome to the react course</div>
+      </>
+    );
+
+  return <h1>Hello World</h1>;
+};
 
 export default Message;
