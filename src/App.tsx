22a708a2ba595ceed649ad862479e4f779af9be4
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-05-30 18:07:33 +0530

    passing children via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import Message from "./Components/Message";
 import ListGroup from "./Components/ListGroup";
+import Alert from "./Components/Alert";
+
+// function App() {
+//   let colors = ["red", "blue", "green", "brown", "yellow"];
+//   let heading = "Colors";
+
+//   const handleSelected = (items: string) => console.log(items);
+//   return (
+//     <span>
+//       <ListGroup
+//         items={colors}
+//         heading={heading}
+//         selectedItem={handleSelected}
+//       />
+//     </span>
+//   );
+// }
 
 function App() {
-  let colors = ["red", "blue", "green", "brown", "yellow"];
-  let heading = "Colors";
-
-  const handleSelected = (items: string) => console.log(items);
+  const message = "Hello Akshay";
   return (
-    <span>
-      <ListGroup
-        items={colors}
-        heading={heading}
-        selectedItem={handleSelected}
-      />
-    </span>
+    <div>
+      <Alert>
+        {message} <p>Welcome to React</p>
+      </Alert>
+    </div>
   );
 }
 
