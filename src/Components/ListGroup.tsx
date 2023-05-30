c52687249a2a748b0c486bb8ec4ddf1c955a1ac8
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-05-30 16:16:45 +0530

    added click event

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 
 const ListGroup = () => {
   let cities = ["Bangalore", "Hyderabad", "Chennai", "Mysore", "Gurgaon"];
-  //cities = [];
+
+  const handleClick = (event: MouseEvent) => console.log(event.type);
 
   return (
     <>
       <h1>List of cities</h1>
       {cities.length === 0 && <p>No city found</p>}
       <ul className="list-group">
-        {cities.map((city) => (
-          <li className="list-group-item" key={city}>
+        {cities.map((city, index) => (
+          <li className="list-group-item" key={city} onClick={handleClick}>
             {city}
           </li>
         ))}
