364c43e0bbcec655fac5e8c45b8e5535ffca181a
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-03 10:03:05 +0530

    form using useState hook

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,11 +1,16 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
   //   const nameRef = useRef<HTMLInputElement>(null);
   //   const ageRef = useRef<HTMLInputElement>(null);
   //   const person = { name: "", age: 0 };
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
+    console.log(person);
     // if (nameRef.current != null) {
     //   person.name = nameRef.current.value;
     // }
@@ -20,13 +25,29 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
